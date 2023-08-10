9974100b7375f3218cfa259fab99f327370075da
Starc151 <dr.Starc@ya.ru>
2023-08-10 14:03:44 +0300

    test compilation apk

---
+++ main.go
@@ -0,0 +1,18 @@
+// compilation for Android
+// fyne package -os android -appID ru.dia.android -icon img/icon.png
+
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("Dia")
+	icon, _ := fyne.LoadResourceFromPath("img/icon.png")
+	app.SetIcon(icon)
+
+	window.ShowAndRun()
+}
