29ad8ea108e8f05387f8eeadb57077134eb20553
Starc151 <dr.Starc@ya.ru>
2023-08-21 22:14:51 +0300

    add tabs and menu

--- pkgs/apk/apk.go
+++ pkgs/apk/apk.go
@@ -54,41 +54,52 @@ func (a *Apk) LoadApk(app fyne.App) {
 
 	a.window = app.NewWindow("Dia")
 
-	a.window.SetContent(container.NewGridWithColumns(1,
-		container.NewGridWithColumns(2,
-			a.input("Entry:", "left"),
-			a.input("ok", "right")),
-		container.NewGridWithColumns(2,
-			a.input("Glucose:", "left"),
-			a.input("11", "right")),
-		container.NewGridWithColumns(2,
-			a.input("B Unit:", "left"),
-			a.input("2", "right")),
+	tabs := container.NewAppTabs(
+		container.NewTabItem("BOLUS",
 		container.NewGridWithColumns(1,
-			a.input("BOLUS: 3", "central")),
-		container.NewGridWithColumns(3,
-			a.charBtn("GL"),
-			a.charBtn("BU"),
-			a.charBtn("OK")),
-		container.NewGridWithColumns(3,
-			a.digitBtn(7),
-			a.digitBtn(8),
-			a.digitBtn(9)),
-		container.NewGridWithColumns(3,
-			a.digitBtn(4),
-			a.digitBtn(5),
-			a.digitBtn(6)),
-		container.NewGridWithColumns(3,
-			a.digitBtn(1),
-			a.digitBtn(2),
-			a.digitBtn(3)),
-		container.NewGridWithColumns(3,
-				a.charBtn("<"),
-				a.digitBtn(0),
-				a.charBtn(".,")),
+			container.NewGridWithColumns(2,
+				a.input("Entry:", "left"),
+				a.input("ok", "right")),
+			container.NewGridWithColumns(2,
+				a.input("Glucose:", "left"),
+				a.input("11", "right")),
+			container.NewGridWithColumns(2,
+				a.input("B Unit:", "left"),
+				a.input("2", "right")),
+			container.NewGridWithColumns(1,
+				a.input("BOLUS: 3", "central")),
+			container.NewGridWithColumns(3,
+				a.charBtn("GL"),
+				a.charBtn("BU"),
+				a.charBtn("OK")),
+			container.NewGridWithColumns(3,
+				a.digitBtn(7),
+				a.digitBtn(8),
+				a.digitBtn(9)),
+			container.NewGridWithColumns(3,
+				a.digitBtn(4),
+				a.digitBtn(5),
+				a.digitBtn(6)),
+			container.NewGridWithColumns(3,
+				a.digitBtn(1),
+				a.digitBtn(2),
+				a.digitBtn(3)),
+			container.NewGridWithColumns(3,
+					a.charBtn("<"),
+					a.digitBtn(0),
+					a.charBtn(".,")),
+			),
 		),
 	)
-
+	fullHistory := fyne.NewMenuItem("", nil)
+	settings := fyne.NewMenuItem("", nil)
+	
+	menuF := fyne.NewMenu("Full History", fullHistory)
+	menuH := fyne.NewMenu("Settings", settings)
+	mainMenu := fyne.NewMainMenu(menuF, menuH)
+	
+	a.window.SetMainMenu(mainMenu)
+	a.window.SetContent(tabs)
 	a.window.Resize(fyne.NewSize(200, 300))
 	a.window.Show()
 }
