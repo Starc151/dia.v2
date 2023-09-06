dcdebedf1fd6db8208df8f509a81acab12b30026
Starc151 <dr.starc@ya.ru>
2023-09-06 16:40:24 +0300

    fix name struct

--- pkgs/apk/apk.go
+++ pkgs/apk/apk.go
@@ -11,68 +11,66 @@ import (
 	"github.com/Starc151/dia.v2/pkgs/bolus"
 )
 
-type Apk struct {
+type glucometr struct {
 	entry     *canvas.Text
 	btn       *widget.Button
 	window    fyne.Window
-	glucometr struct {
-		glucose *canvas.Text
-		bUnit   *canvas.Text
-		bolus   *canvas.Text
-	}
+	glucose *canvas.Text
+	bUnit   *canvas.Text
+	bolus   *canvas.Text
 }
 
-func (a *Apk) digitBtn(number int) *widget.Button {
+func (g *glucometr) digitBtn(number int) *widget.Button {
 	str := strconv.Itoa(number)
-	return a.addBtn(str, func() {
-		a.setEntry(str)
+	return g.addBtn(str, func() {
+		g.setEntry(str)
 	})
 }
 
-func (a *Apk) setGlucometr(text string, field *canvas.Text) {
+func (g *glucometr) setGlucometr(text string, field *canvas.Text) {
 	field.Text = text
 	field.Refresh()
-	a.entry.Text = "_"
-	a.entry.Refresh()
+	g.entry.Text = "_"
+	g.entry.Refresh()
 }
 
-func (a *Apk) addBtn(text string, action func()) *widget.Button {
-	a.btn = &widget.Button{}
-	a.btn.Text = text
-	a.btn.OnTapped = action
-	return a.btn
+func (g *glucometr) addBtn(text string, action func()) *widget.Button {
+	g.btn = &widget.Button{}
+	g.btn.Text = text
+	g.btn.OnTapped = action
+	return g.btn
 }
 
-func (a *Apk) setEntry(text string) {
-	if a.entry.Text == "_" {
-		a.entry.Text = ""
+func (g *glucometr) setEntry(text string) {
+	if g.entry.Text == "_" {
+		g.entry.Text = ""
 	}
-	if len(a.entry.Text) < 4 {
-		a.entry.Text += text
+	if len(g.entry.Text) < 4 {
+		g.entry.Text += text
 	}
-	a.entry.Refresh()
+	g.entry.Refresh()
 }
 
-func (a *Apk) clear() {
-	a.entry.Text = "_"
-	a.glucometr.glucose.Text = "0"
-	a.glucometr.bUnit.Text = "0"
-	a.glucometr.bolus.Text = ""
-	a.entry.Refresh()
-	a.glucometr.glucose.Refresh()
-	a.glucometr.bUnit.Refresh()
-	a.glucometr.bolus.Refresh()
+func (g *glucometr) clear() {
+	g.entry.Text = "_"
+	g.glucose.Text = "0"
+	g.bUnit.Text = "0"
+	g.bolus.Text = ""
+	g.entry.Refresh()
+	g.glucose.Refresh()
+	g.bUnit.Refresh()
+	g.bolus.Refresh()
 }
 
-func (a *Apk) backSpace() {
-	len := len(a.entry.Text)
+func (g *glucometr) backSpace() {
+	len := len(g.entry.Text)
 	if len == 1 {
-		a.entry.Text = "_"
-		a.entry.Refresh()
+		g.entry.Text = "_"
+		g.entry.Refresh()
 		return
 	}
-	a.entry.Text = a.entry.Text[:len-1]
-	a.entry.Refresh()
+	g.entry.Text = g.entry.Text[:len-1]
+	g.entry.Refresh()
 }
 
 func setAlign(position string) int {
@@ -86,31 +84,31 @@ func setAlign(position string) int {
 	return positionNum
 }
 
-func (a *Apk) setCanvasText(text, position string) *canvas.Text {
+func (g *glucometr) setCanvasText(text, position string) *canvas.Text {
 	canvasText := &canvas.Text{Color: color.White, TextSize: 25}
 	canvasText.Text = text
 	canvasText.Alignment = fyne.TextAlign(setAlign(position))
 	return canvasText
 }
 
-func (a *Apk) getBolus() {
-	glucose := a.glucometr.glucose.Text
-	bUnit := a.glucometr.bUnit.Text
+func (g *glucometr) getBolus() {
+	glucose := g.glucose.Text
+	bUnit := g.bUnit.Text
 	bolus := bolus.SetGlucometr(glucose, bUnit)
 
-	a.glucometr.bolus.Text = fmt.Sprintf("Bolus: %.1f", bolus)
-	a.glucometr.bolus.Refresh()
+	g.bolus.Text = fmt.Sprintf("Bolus: %.1f", bolus)
+	g.bolus.Refresh()
 }
 
 func NewGlucometr(app fyne.App) {
-	a := Apk{}
+	g := glucometr{}
 	app.Settings().SetTheme(&myTheme{})
-	a.window = app.NewWindow("Dia")
-	a.entry = a.setCanvasText("_", "right")
-	a.glucometr.bUnit = a.setCanvasText("0", "right")
-	a.glucometr.glucose = a.setCanvasText("0", "right")
-	a.glucometr.bolus = a.setCanvasText("", "centr")
+	g.window = app.NewWindow("Dia")
+	g.entry = g.setCanvasText("_", "right")
+	g.bUnit = g.setCanvasText("0", "right")
+	g.glucose = g.setCanvasText("0", "right")
+	g.bolus = g.setCanvasText("", "centr")
 
-	a.loadApk()
-	a.window.Show()
+	g.loadApk()
+	g.window.Show()
 }
